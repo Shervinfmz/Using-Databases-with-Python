bf02a4f9b5724f9621de97a4305412bbbb2f0a7b
Shervin Faramarzi <faramarzi.shervin@gmail.com>
2026-01-06 16:54:08 +0330

    .

---
+++ DatabasesAndVisualiztion/opengeo/where.js
@@ -0,0 +1,9 @@
+myData = [
+[50.065703299999996,19.918958667058632, 'AGH University of Science and Technology'],
+[52.2394966,21.015693129786936, 'Academy of Fine Arts in Warsaw'],
+[30.04281175,31.23658590813949, 'American University in Cairo'],
+[33.4213174,-111.93316305413154, 'Arizona State University'],
+[37.9425172,23.8706149, 'Athens Information Technology'],
+[17.547185,78.572791, 'BITS Pilani'],
+[6.8900558499999995,3.722437696387937, 'Babcock University']
+];
